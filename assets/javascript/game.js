cc9f4ace4bfd99db730869a3a27334b46f135a23
Mickey S <marimikaels@gmail.com>
2017-03-15 05:20:41 -0400

    special win/loss animations and sounds added in; foolproofing it so user cant mess up the game by clicking the crystals while the animations and sounds are playing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,22 +6,25 @@ var redCrystal;
 var yellowCrystal;
 var blueCrystal;
 var greenCrystal;
-var inCurrentRound = false;
+var inCurrentRound = true;
 var newValsArray;
+
+
+
 
 
 //set 4 unique values between 1 and 12 inclusive 
 function gemValue() {
     var blankarray = [];
-    for (i = 1; i<1000; i++) {
-    	if (blankarray.length >= 4) {
-    		return blankarray;
+    for (i = 1; i < 1000; i++) {
+        if (blankarray.length >= 4) {
+            return blankarray;
 
-    	}
+        }
         var myVal = Math.floor((Math.random() * 12) + 1);
         if (blankarray.indexOf(myVal) === -1) {
 
-        	blankarray.push(myVal);
+            blankarray.push(myVal);
 
 
         }
@@ -50,14 +53,14 @@ function gemValue() {
 
 //assign values to each of the 4 crystals
 function setAllValues(myArray) {
-	redCrystal = myArray[0];
-	console.log(redCrystal);
-	yellowCrystal = myArray[1];
-	console.log(yellowCrystal);
-	blueCrystal = myArray[2];
-	console.log(blueCrystal);
-	greenCrystal = myArray[3];
-	console.log(greenCrystal);
+    redCrystal = myArray[0];
+    console.log(redCrystal);
+    yellowCrystal = myArray[1];
+    console.log(yellowCrystal);
+    blueCrystal = myArray[2];
+    console.log(blueCrystal);
+    greenCrystal = myArray[3];
+    console.log(greenCrystal);
 }
 
 
@@ -67,20 +70,24 @@ function gameEnd() {
     console.log("game over");
     playerScore = 0;
     $("#playerScore").html("Total Score: " + playerScore);
-    inCurrentRound = false;
+    
     targetNumber = Math.floor((Math.random() * 120) + 19);
     $("#targetNumber").html("Try to match this number: " + targetNumber);
-    $("#winslosses").html("Wins: " + wins + "<br>" + "Losses: " + losses );
+    $("#winslosses").html("Wins: " + wins + "<br>" + "Losses: " + losses);
+    // $("#sonicpic").html("<img src = 'assets/images/sonic1.png' id = 'sonicpic' alt = 'sonic'>");
     // $("#losses").html("Losses: " + losses);
+    $("#sonicpic").attr("src", "assets/images/sonic1.png");
+    $("#footer").empty();
     newValsArray = gemValue();
 
     setAllValues(newValsArray);
+    inCurrentRound = true;
 }
 
 
 function playGame() {
 
-	newValsArray = gemValue();
+    newValsArray = gemValue();
 
     setAllValues(newValsArray);
     $("#playerScore").html("Total Score: " + playerScore);
@@ -101,69 +108,132 @@ function playGame() {
 
     // }
 
+    // if (inCurrentRound === true) {  
+
 
     //when each crystal button is clicked, add the corresponding crystal value to the player's score, and check for a win or loss
+    //when win or loss occurs, play special music and display special picture for as many seconds as the sound effect lasts, then automatically start a new game
+    //user will be locked out of clicking the crystals while the special picture and music play
     $("#redcrystal").on("click", function() {
-        playerScore = playerScore + redCrystal;
-        $("#playerScore").html("Total Score: " + playerScore);
-        if (playerScore > targetNumber) {
+        if (inCurrentRound === true) {
+            playerScore = playerScore + redCrystal;
+            $("#playerScore").html("Total Score: " + playerScore);
+        }
+        if (playerScore > targetNumber && inCurrentRound === true) {
+
+            inCurrentRound = false;
 
             losses++;
-            gameEnd();
+            $("#gameOutcome").attr("src", "assets/music/gameover.mp3");
+            document.getElementById('gameOutcome').play();
+            $("#sonicpic").attr("src", "assets/images/eggman.png");
+            $("#footer").append("BETTER LUCK NEXT TIME!");
+            setTimeout(gameEnd, 12500);
 
-        } else if (playerScore === targetNumber) {
+
+        } else if (playerScore === targetNumber && inCurrentRound === true) {
+
+            inCurrentRound = false;
 
             wins++;
-            gameEnd();
+            $("#gameOutcome").attr("src", "assets/music/supersonic.mp3");
+            document.getElementById('gameOutcome').play();
+            $("#sonicpic").attr("src", "assets/images/supersonic2.png");
+            $("#footer").append("YOU WIN! BECOME SUPER SONIC!");
+            setTimeout(gameEnd, 5500);
         }
     });
 
     $("#yellowcrystal").on("click", function() {
-        playerScore = playerScore + yellowCrystal;
-        $("#playerScore").html("Total Score: " + playerScore);
-        if (playerScore > targetNumber) {
+
+        if (inCurrentRound === true) {
+            playerScore = playerScore + yellowCrystal;
+            $("#playerScore").html("Total Score: " + playerScore);
+        }
+        if (playerScore > targetNumber && inCurrentRound === true) {
+
+            inCurrentRound = false;
 
             losses++;
-            gameEnd();
+            $("#gameOutcome").attr("src", "assets/music/gameover.mp3");
+            document.getElementById('gameOutcome').play();
+            $("#sonicpic").attr("src", "assets/images/eggman.png");
+            $("#footer").append("BETTER LUCK NEXT TIME!");
+            setTimeout(gameEnd, 12500);
 
-        } else if (playerScore === targetNumber) {
+        } else if (playerScore === targetNumber && inCurrentRound === true) {
+
+            inCurrentRound = false;
 
             wins++;
-            gameEnd();
+            $("#gameOutcome").attr("src", "assets/music/supersonic.mp3");
+            document.getElementById('gameOutcome').play();
+            $("#sonicpic").attr("src", "assets/images/supersonic2.png");
+            $("#footer").append("YOU WIN! BECOME SUPER SONIC!");
+            setTimeout(gameEnd, 5500);
         }
     });
 
 
     $("#bluecrystal").on("click", function() {
-        playerScore = playerScore + blueCrystal;
 
-        $("#playerScore").html("Total Score: " + playerScore);
-        if (playerScore > targetNumber) {
+        if (inCurrentRound === true) {
+            playerScore = playerScore + blueCrystal;
+
+            $("#playerScore").html("Total Score: " + playerScore);
+        }
+        if (playerScore > targetNumber && inCurrentRound === true) {
+
+            inCurrentRound = false;
 
             losses++;
-            gameEnd();
+            $("#gameOutcome").attr("src", "assets/music/gameover.mp3");
+            document.getElementById('gameOutcome').play();
+            $("#sonicpic").attr("src", "assets/images/eggman.png");
+            $("#footer").append("BETTER LUCK NEXT TIME!");
+            setTimeout(gameEnd, 12500);
 
-        } else if (playerScore === targetNumber) {
+        } else if (playerScore === targetNumber && inCurrentRound === true) {
+
+            inCurrentRound = false;
 
             wins++;
-            gameEnd();
+            $("#gameOutcome").attr("src", "assets/music/supersonic.mp3");
+            document.getElementById('gameOutcome').play();
+            $("#sonicpic").attr("src", "assets/images/supersonic2.png");
+            $("#footer").append("YOU WIN! BECOME SUPER SONIC!");
+            setTimeout(gameEnd, 5500);
         }
     });
 
     $("#greencrystal").on("click", function() {
 
-        playerScore = playerScore + greenCrystal;
+        if (inCurrentRound === true) {
 
-        $("#playerScore").html("Total Score: " + playerScore);
-        if (playerScore > targetNumber) {
+            playerScore = playerScore + greenCrystal;
+
+            $("#playerScore").html("Total Score: " + playerScore);
+        }
+        if (playerScore > targetNumber && inCurrentRound === true) {
+
+            inCurrentRound = false;
 
             losses++;
-            gameEnd();
+            $("#gameOutcome").attr("src", "assets/music/gameover.mp3");
+            document.getElementById('gameOutcome').play();
+            $("#sonicpic").attr("src", "assets/images/eggman.png");
+            $("#footer").append("BETTER LUCK NEXT TIME!");
+            setTimeout(gameEnd, 12500);
 
-        } else if (playerScore === targetNumber) {
+        } else if (playerScore === targetNumber && inCurrentRound === true) {
+            inCurrentRound = false;
 
             wins++;
-            gameEnd();
+            $("#gameOutcome").attr("src", "assets/music/supersonic.mp3");
+            document.getElementById('gameOutcome').play();
+            $("#sonicpic").attr("src", "assets/images/supersonic2.png");
+            $("#footer").append("YOU WIN! BECOME SUPER SONIC!");
+            setTimeout(gameEnd, 5500);
         }
     });
 

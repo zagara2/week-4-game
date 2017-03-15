6501691cfd496f4436b2eb67f0ae07dc93b5dcc2
Mickey S <marimikaels@gmail.com>
2017-03-14 21:18:37 -0400

    game logic is fully functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var targetNumber = Math.floor((Math.random() * 120) + 19);
+var targetNumber = Math.floor((Math.random() * 120) + 19); // generate number between 19 and 120
 var playerScore = 0;
 var wins = 0;
 var losses = 0;
@@ -7,30 +7,64 @@ var yellowCrystal;
 var blueCrystal;
 var greenCrystal;
 var inCurrentRound = false;
+var newValsArray;
 
 
-//set a value between 1 and 12 inclusive 
+//set 4 unique values between 1 and 12 inclusive 
 function gemValue() {
-    var myVal = Math.floor((Math.random() * 12) + 1);
-    return myVal;
+    var blankarray = [];
+    for (i = 1; i<1000; i++) {
+    	if (blankarray.length >= 4) {
+    		return blankarray;
+
+    	}
+        var myVal = Math.floor((Math.random() * 12) + 1);
+        if (blankarray.indexOf(myVal) === -1) {
+
+        	blankarray.push(myVal);
+
+
+        }
+
+    }
+    //return myVal;
+    return blankarray;
 }
 
 //give each gem variable a value between 1 and 12 if a round is not already going, and then signify that a round is going after the crystal values for the new round are set
-function setAllValues() {
-    if (inCurrentRound === false) {
-        redCrystal = gemValue();
-        yellowCrystal = gemValue();
-        blueCrystal = gemValue();
-        greenCrystal = gemValue();
-        inCurrentRound = true;
-    }
-    
+// function setAllValues() {
+//     if (inCurrentRound === false) {
+//         redCrystal = gemValue();
+//         console.log(redCrystal);
+//         yellowCrystal = gemValue();
+//         console.log(yellowCrystal);
+//         blueCrystal = gemValue();
+//         console.log(blueCrystal);
+//         greenCrystal = gemValue();
+//         console.log(greenCrystal);
+//         inCurrentRound = true;
+//     }
+
+// }
+
+
+//assign values to each of the 4 crystals
+function setAllValues(myArray) {
+	redCrystal = myArray[0];
+	console.log(redCrystal);
+	yellowCrystal = myArray[1];
+	console.log(yellowCrystal);
+	blueCrystal = myArray[2];
+	console.log(blueCrystal);
+	greenCrystal = myArray[3];
+	console.log(greenCrystal);
 }
 
 
 
 
 function gameEnd() {
+    console.log("game over");
     playerScore = 0;
     $("#playerScore").html("Your Total Score: " + playerScore);
     inCurrentRound = false;
@@ -38,49 +72,102 @@ function gameEnd() {
     $("#targetNumber").html("Try to match this number: " + targetNumber);
     $("#wins").html("Wins: " + wins);
     $("#losses").html("Losses: " + losses);
+    newValsArray = gemValue();
 
-    setAllValues();
+    setAllValues(newValsArray);
 }
 
 
 function playGame() {
 
-    setAllValues();
+	newValsArray = gemValue();
+
+    setAllValues(newValsArray);
     $("#playerScore").html("Your Total Score: " + playerScore);
     $("#targetNumber").html("Try to match this number: " + targetNumber);
 
-    if (playerScore > targetNumber) {
-        
-        losses++;
-        gameEnd();
+    // if (inCurrentRound === true) {
 
-    } else if (playerScore === targetNumber) {
-        
-        wins++;
-        gameEnd();
-    }
+    //     if (playerScore > targetNumber) {
+
+    //         losses++;
+    //         gameEnd();
+
+    //     } else if (playerScore === targetNumber) {
+
+    //         wins++;
+    //         gameEnd();
+    //     }
+
+    // }
 
 
-    //when each crystal button is clicked, add the corresponding crystal value to the player's score
+    //when each crystal button is clicked, add the corresponding crystal value to the player's score, and check for a win or loss
     $("#redcrystal").on("click", function() {
         playerScore = playerScore + redCrystal;
+        $("#playerScore").html("Your Total Score: " + playerScore);
+        if (playerScore > targetNumber) {
+
+            losses++;
+            gameEnd();
+
+        } else if (playerScore === targetNumber) {
+
+            wins++;
+            gameEnd();
+        }
     });
 
     $("#yellowcrystal").on("click", function() {
         playerScore = playerScore + yellowCrystal;
+        $("#playerScore").html("Your Total Score: " + playerScore);
+        if (playerScore > targetNumber) {
+
+            losses++;
+            gameEnd();
+
+        } else if (playerScore === targetNumber) {
+
+            wins++;
+            gameEnd();
+        }
     });
 
 
     $("#bluecrystal").on("click", function() {
         playerScore = playerScore + blueCrystal;
+
+        $("#playerScore").html("Your Total Score: " + playerScore);
+        if (playerScore > targetNumber) {
+
+            losses++;
+            gameEnd();
+
+        } else if (playerScore === targetNumber) {
+
+            wins++;
+            gameEnd();
+        }
     });
 
     $("#greencrystal").on("click", function() {
 
         playerScore = playerScore + greenCrystal;
+
+        $("#playerScore").html("Your Total Score: " + playerScore);
+        if (playerScore > targetNumber) {
+
+            losses++;
+            gameEnd();
+
+        } else if (playerScore === targetNumber) {
+
+            wins++;
+            gameEnd();
+        }
     });
 
-    
+
 
 
 }
